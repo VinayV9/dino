4455f3052be6f0fccef57b7dedc8218c9a52f748
VinayV9 <vinayveeren@gmail.com>
2018-07-13 15:40:57 +0530

    ducking dino added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+/**
+ * when dom loads move obstacles call for every 2 seconds
+ * then and it's listen to key strokes on keyboard
+ * inside keydown event
+ * when user press tab or up arrow 
+ * dino img will animate top to bottom
+ * @param {*} e listing on keyboard events
+ */  
 $(document).ready(function(){
    console.log("loaded");
    setInterval("moveObstacles()", 2000);
@@ -12,13 +20,11 @@ $(document).ready(function(){
                 top: '-80px'
             }, 400);     
     }
+    if(key === 40){
+        setTimeout('toogleDino(1)', 0);
+        setTimeout('toogleDino(0)', 200);
+    }
 });
-/**
- * when user press tab or up arrow 
- * dino img will animate top to bottom
- * @param {*} e listing on keyboard events
- */  
-
  /**
   *  we have seven bushes 
   * each one is 50px wide
@@ -42,7 +48,11 @@ const moveObstacles = () => {
             $(this).remove();            
       });
 };
-
+/**
+ * checks for hit btw
+ * dino and bush
+ * if it's the bush alert will call with you loose messg
+ */
 const checkCollision = () => {
     var pos1=$(".obstacles").position();
     var pos2=$(".dino-img").position();
@@ -50,4 +60,13 @@ const checkCollision = () => {
     if((pos1.left > -40 && pos1.left < 160) && (pos2.top >= -80 && pos2.top < 130)){
         alert('you lose');
     }
+}
+const toogleDino = (state) => {
+    if(state === 1){
+        $(".dino-img").remove();
+        $("#game-ground").append('<img src="images/duck.gif" alt="ducking dino" class="dino-img">');
+    }else{
+        $(".dino-img").remove();
+        $("#game-ground").append('<img src="images/run1.gif" alt="dino" class="dino-img">')
+    } 
 }
